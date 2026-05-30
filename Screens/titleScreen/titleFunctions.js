13ba59ea7e71a342d5dd5525beba2ee7f958835d
Your Name <you@example.com>
2026-05-29 21:50:24 -0700

    Created UI skeleton for title screen

---
+++ Screens/titleScreen/titleFunctions.js
@@ -0,0 +1,6 @@
+/* Code for settings button */
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('.settings-button').addEventListener('click', function() {
+        window.location.href = 'settings.html';
+    });
+});
