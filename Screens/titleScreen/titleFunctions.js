51f6714e4ad0c68d8a550758199eb832e97b1328
Your Name <you@example.com>
2026-05-31 15:53:46 -0700

    Created settings page and updated titlescreen

--- Screens/titleScreen/titleFunctions.js
+++ Screens/titleScreen/titleFunctions.js
@@ -1,6 +1,14 @@
 /* Code for settings button */
 document.addEventListener('DOMContentLoaded', function() {
     document.querySelector('.settings-button').addEventListener('click', function() {
-        window.location.href = 'settings.html';
+        window.location.href = '/Screens/settings/settings.html';
+        console.log("here");
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('.credits-button').addEventListener('click', function() {
+        window.location.href = '/Screens/credits/credits.html';
+        console.log("here");
     });
 });
