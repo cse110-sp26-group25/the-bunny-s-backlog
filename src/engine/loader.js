5267ddf34b15722bfbba578c5e1e303524ce0fa9
pranav <prganesan@ucsd.edu>
2026-06-06 14:03:22 -0700

    Change level file from 'levels.json' to 'level0.json'

--- src/engine/loader.js
+++ src/engine/loader.js
@@ -31,7 +31,7 @@ class LevelLoader {
    */
   async loadLevel(folder) {
     const folderUrl = this.levelFolderUrl(folder);
-    const level = await LevelLoader.fetchJson(folderUrl + "levels.json");
+    const level = await LevelLoader.fetchJson(folderUrl + "level0.json");
     LevelLoader.validateLevel(level, folder);
 
     return LevelLoader.resolveAssets(level, folderUrl);
